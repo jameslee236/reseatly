f3fc9ff6c04f304836e9b60ff094855e1813b17c
Jameslee236 <jameslee236@gmail.com>
2016-01-12 09:53:48 -0800

    Refactors Seat model

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -4,16 +4,4 @@ class Seat < ActiveRecord::Base
 	belongs_to :owner, class_name: "User"
 	validates :owner_id, uniqueness: true
 	validate :owner_id_greater_than_zero
-
-	def owner_id_greater_than_zero
-		if owner_id < 0
-			errors.add(:seat, "Owner does not exist")
-		end
-	end
-
-	def owner_must_exist
-		if User.find_by(user_id: owner_id).nil?
-			errors.add(:seat, "The owner you're trying to assign doesn't exist")
-		end
-	end
 end
