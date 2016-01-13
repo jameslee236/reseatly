f83932949042aa1f7f2f3d5da6575a8c8d01b5cf
Jameslee236 <jameslee236@gmail.com>
2016-01-13 15:36:28 -0800

    Adds segment pixel

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -3,5 +3,4 @@ class Seat < ActiveRecord::Base
 	has_many :users, :through => :records
 	belongs_to :owner, class_name: "User"
 	validates :owner_id, uniqueness: true
-	validate :owner_id_greater_than_zero
 end
